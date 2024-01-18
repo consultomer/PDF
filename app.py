76033183cdead5f12b94d01212a4fade15ee7f94
Omer Abdul Rehman <consultomer@gmail.com>
2024-01-18 16:06:48 +0500

    Fisrt commit

---
+++ app.py
@@ -0,0 +1,28 @@
+from flask import Flask
+from app import app
+from flask import jsonify, send_file, send_from_directory, render_template
+import threading
+import os
+
+
+app = Flask(__name__)
+
+
+pdf_folder = 'books'
+@app.route("/books", methods=['GET'])
+def list_pdfs():
+    pdf_files = [f for f in os.listdir(pdf_folder) if f.endswith('.pdf')]
+    
+    # Create a list of links with href attributes
+    pdf_links = [{'filename': file, 'link': f'/books/{file}'} for file in pdf_files]
+
+    return render_template('index.html', pdf_links=pdf_links)
+
+
+@app.route("/books/<filename>", methods=['GET'])
+def download_pdf(filename):
+    # Ensure the requested file is within the 'books' folder and has a .pdf extension
+    if not filename.endswith('.pdf') or '..' in filename:
+        return "Invalid request", 400
+
+    return send_from_directory(pdf_folder, filename, as_attachment=True)
---
+++ readme.md
@@ -0,0 +1,17 @@
+python3 -m venv myenv
+    ```
+3. Activate the virtual environment:
+    - For Windows:
+      ```shell
+      myenv\Scripts\activate
+      ```
+    - For macOS/Linux:
+      ```shell
+      source myenv/bin/activate
+      ```
+4. Install Flask using pip:
+    ```shell
+    pip install flask
+    ```
+
+Once Flask is installed, you can add a link to your books in your Flask app. Here's an example of how you can do it:
